38e94b26053067e114cca91cccde3f509a9125a2
Martin <etek.martin@gmail.com>
2026-03-11 20:37:49 -0400

    2 space indent test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    int x = 5;
-    std::cout << "Hello Ninja! X = " << x << std::endl;
-    return 0;
+  int x = 5;
+  std::cout << "Hello Ninja! X = " << x << std::endl;
+  return 0;
 }
